02ce3b66318a66e7b0a419fb3d181b284c326d49
pablo clavero <pabloclavero03@gmail.com>
2022-05-11 13:35:38 -0300

    v3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -125,6 +125,13 @@ return numero % 1 === 0;
     //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
     //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
     //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+if (num1 < 0 || num2 < 0 || num2 < 0) return 'Hay negativos';
+if (num1 === 0 || num2 === 0 || num3 === 0) return 'Error';
+if (num1 > num2 && num1 > num3 && num1 > 0) return 'Número 1 es mayor y positivo';
+if (num3 > num1 && num3 > num2 ) return ++num3;
+return false;
+  
+  
   }
   
   function esPrimo(numero) {
@@ -133,26 +140,43 @@ return numero % 1 === 0;
     // Pista: un número primo solo es divisible por sí mismo y por 1
     // Pista 2: Puedes resolverlo usando un bucle `for`
     // Nota: Los números 0 y 1 NO son considerados números primos
+  if ( numero < 2) return false
+  if ( numero === 2) return true
+  for (var i = 2 ; i < numero; i++) {
+      if (numero % i === 0) {
+        return false
+    }
   }
+  return true
+}
   
   function esVerdadero(valor){
     //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
     //si su valor es true y “Soy falso” si su valor es false.
     //Escribe tu código aquí
-  
+   if (valor === true) return "Soy verdadero"
+    return "Soy falso"
   }
   
   function tablaDelSeis(){
     //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
     //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
     //Escribe tu código aquí   
-    
+  var arraytablaDelSeis = []
+  for (var i = 0 ; i< 11; i++) {
+    arraytablaDelSeis.push(6 * i)
+  }
+  return arraytablaDelSeis
+
+
+
   }
   
   function tieneTresDigitos(numero){
     //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
     //Escribe tu código aquí
-    
+    if (numero > 99 && numero <1000) return true ;
+    return false;
   }
   
   function doWhile(numero) {
